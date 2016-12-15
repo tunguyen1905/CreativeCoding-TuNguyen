8094ad494680c13470468226ca3b76b1852bc981
tunguyen1905 <tn896@nyu.edu>
2016-12-15 05:55:32 -0500

    Add files via upload

---
+++ Week 2 Homework.js
@@ -0,0 +1,31 @@
+//Tu Nguyen
+//Week 2 Homework
+//Artwork: Study for Homage to the Square: Still Remembered
+//Artist: Albers Josef
+//Year 1956
+
+var gap = 100;
+var distance = 40;
+  
+function setup () {
+  createCanvas(400,400);
+
+}
+
+function draw (){
+  background(253,253,249);
+  noStroke();
+  
+  fill(8,88,83);
+  rect(10,10,width-20,height-20);
+
+  fill(252,238,39);
+  rect(10+distance,10+distance*1.5,width-gap,height-gap);
+  
+  fill(201,191,62);
+  rect(10+distance*2.25,10+distance*3.5,width-gap*2,height-gap*2);
+  
+  fill(70,50,10);
+  rect(10+distance*3.5,10+distance*5.5,width-gap*3,height-gap*3);
+}
+  
